00150ecda62468408d5b65c2f4599edc627c4206
bryan@wangl <bryanwangl@bryanwangldeMacBook-Air.local>
2024-03-22 15:11:33 +0800

    去除不必要的 import 信息

--- tlv/Main.java
+++ tlv/Main.java
@@ -1,17 +1,12 @@
-import javax.xml.transform.stream.StreamSource;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Vector;
-import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
         String emv = "911001020304050607080910111213141516720E1111222222222233334444444444";
-        byte[] bin = null;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        TLVSerializer writer = new TLVSerializer(out);
+        byte[] bin;
         try {
             bin = TLVUtils.hex2Bin(emv);
         } catch (TLVParserException e) {
@@ -19,7 +14,7 @@ public class Main {
         }
         InputStream input = new ByteArrayInputStream(bin);
         TLVParser parser = new TLVParser(input);
-        Vector<TLVElement> elements = new Vector<TLVElement>();
+        Vector<TLVElement> elements;
         try {
             elements = parser.parseAllTLVElement();
             System.out.println(elements.size());
